6fd75c8c607f18e7f301a6ee49db9271ad94ff75
Ivanyany <754339948@qq.com>
2020-03-26 10:20:18 +0800

    Initial commit mybatis-plus-version3

---
+++ src/main/resources/tb_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO mp_user (name, age, email) VALUES
+('Tom', 18, 'Tom@ivan.com'),
+('Jerry', 20, 'Jerry@ivan.com'),
+('Jack', 28, 'Jack@ivan.com'),
+('Sandy', 21, 'Sandy@ivan.com'),
+('Ivan', 24, 'Ivan@ivan.com');
