b9e5b01c5487905574534c21bf7b1ffeff733f50
Ivanyany <754339948@qq.com>
2020-03-26 10:56:05 +0800

    mp测试通过

--- src/main/java/com/ivan/MybatisPlusApplication.java
+++ src/main/java/com/ivan/MybatisPlusApplication.java
@@ -1,9 +1,11 @@
 package com.ivan;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.ivan.mapper")
 public class MybatisPlusApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/ivan/MybatisPlusApplicationTests.java
+++ src/test/java/com/ivan/MybatisPlusApplicationTests.java
@@ -1,13 +1,25 @@
 package com.ivan;
 
+import com.ivan.bean.User;
+import com.ivan.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class MybatisPlusApplicationTests {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @Test
     void contextLoads() {
+
+        System.out.println(("----- selectAll method test ------"));
+        List<User> userList = userMapper.selectList(null);
+        userList.forEach(System.out::println);
     }
 
 }
